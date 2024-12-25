0fc35ed368cbcaff5298644b76f8d6d5e70eff7f
BilionBoy <dhiogoandre26@gmail.com>
2024-12-25 16:50:36 -0400

    feature: devise admin layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  allow_browser versions: :modern
+
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end

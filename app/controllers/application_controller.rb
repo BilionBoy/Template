924b8df98983c64726b925f600630648074b0485
BilionBoy <dhiogoandre26@gmail.com>
2024-12-27 22:00:32 -0400

    feature: home application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  layout :layout_by_resource
+  layout :layout_by_controller
 
   private
 
-  def layout_by_resource
+  def layout_by_controller
     if devise_controller?
       "devise"
+    elsif controller_name == "home"
+      "home_application"
     else
       "application"
     end
